d89329e318654c67017407a25ef29e429fb68fca
LambdaXVI <bmhavens6@gmail.com>
2019-01-23 19:08:20 -0700

    Version Typo

--- src/main/java/com/github/unassignedxd/voidutils/main/VoidUtils.java
+++ src/main/java/com/github/unassignedxd/voidutils/main/VoidUtils.java
@@ -29,7 +29,7 @@ public class VoidUtils {
 
     public static final String MOD_ID = "voidutils";
     public static final String NAME = "Void Utils";
-    public static final String VERSION = "0.0.1"; //r a b
+    public static final String VERSION = "0.0.1"; //r b a
 
     @Mod.Instance
     public static VoidUtils instance;
